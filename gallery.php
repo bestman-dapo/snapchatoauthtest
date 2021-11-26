063c8bff5497d32d4df100205109362ff875c3de
bestman-dapo <oyebanjioladapo1@gmail.com>
2021-11-26 14:06:15 +0100

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -9,6 +9,7 @@
     <meta property="og:title" content="The Fastest Way to Share!" />
     <meta property="snap:app_id" content="b0cbf6e1-ab6d-4c16-a7e1-b36995a6a38d"/>
     <meta property="snap:publisher_id" content="6657a894-7ddc-49a4-902e-94240dbb6577"/>
+    <meta property="snap:sticker" content="/images/IMG_20211108_091023_868.jpg" />
     <link rel="stylesheet" href="css/gallery.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="./css/bootstrap.min.css">
