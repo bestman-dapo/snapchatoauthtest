3648a816d6936e7ac7dda4b68218bccf4daa2e49
bestman-dapo <oyebanjioladapo1@gmail.com>
2021-11-26 13:56:27 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var login_submit = document.querySelector('.login_submit');
 
 var avatar = document.getElementById('avatar');
 
-var usersname = document.getElementById('usersname');
+var users_name = document.getElementById('usersname');
 
 // window.localStorage.setItem('inneritem', "<div class='index-header'><div onClick='clearStorage()' id='log_out' class='index-header-link' style='cursor:pointer; text-decoration: none; color:white; font-size: 25px; backgorung-color: transparent;' ><span class='gallery-signin'>Log Out</span></div> <div style='display: flex;'><img id='avatar' src=''><div id='usersname'  style='margin-left: 40px;'></div> </div></div>  <section class='gallery min-vh-100 picture-gallery'><div class='container-lg'><div class='row gy-4 row-cols-1 row-cols-sm-2 row-cols-md-3'><div class='col'><img src='images/IMG_20211108_091023_868.jpg' class='gallery-item' alt='gallery'></div> <div class='col'> <img src='images/IMG_20211108_091027_192.jpg' class='gallery-item' alt='gallery'></div> <div class='col'><img src='images/IMG_20211108_091104_614.jpg' class='gallery-item' alt='gallery'></div><div class='col'><img src='images/IMG_20211108_091107_640.jpg' class='gallery-item' alt='gallery'></div><div class='col'><img src='images/IMG_20211108_091115_256.jpg' class='gallery-item' alt='gallery'></div><div class='col'><img src='images/IMG_20211108_091119_950.jpg' class='gallery-item' alt='gallery'></div></div></div></section><div class='modal fade' id='gallery-modal' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'><div class='modal-dialog modal-dialog-centered modal-md'><div class='modal-content'><div class='modal-header'><button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button></div><div class='modal-body'><img src='images/Snapchat-1917058687.jpg' class='modal-img' alt='modal img'></div></div></div></div>");
 
@@ -26,7 +26,7 @@ window.snapKitInit = () => {
         window.localStorage.setItem('username', data["data"]["me"]["displayName"]);
 
         
-        console.log(usersname);
+        console.log(users_name);
         
 
         window.location.href = "https://snapchatoauthtest.herokuapp.com/gallery.php";
